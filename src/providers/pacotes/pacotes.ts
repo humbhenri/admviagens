e2b23d0194847e6ac727d644c4c76285efa738f6
Humberto Pinheiro <humbhenri@gmail.com>
2019-05-04 13:48:40 -0300

    fix storage photo

--- src/providers/pacotes/pacotes.ts
+++ src/providers/pacotes/pacotes.ts
@@ -16,22 +16,26 @@ export class PacotesProvider {
     public http: HttpClient,
     public afd: AngularFireDatabase) {
   }
-
   async addPacote(pacote: any) {
-    const val = await this.afd.list('pacotes').push(pacote);
-    console.log(JSON.stringify(val));
-    const id = val.key;
-    const storage = firebase.storage();
-    const storageRef = storage.ref();
-    const filename = new Date().getTime() + '.png';
-    const localPath = pacote.imagem;
-    const fileType = 'image/png';
-    const savedPicture = await storageRef.child(`pacotes/${id}/${filename}`)
-      .putString(localPath, 'base64', { contentType: fileType });
-    const url = await savedPicture.ref.getDownloadURL();
-    console.log(`img url = ${url}`);
-    pacote.imagem = url;
-    await this.afd.object(`pacotes/${id}`).update(pacote);
+    try {
+      await firebase.auth().signInAnonymously();
+      const val = await this.afd.list('pacotes').push(pacote);
+      console.log(JSON.stringify(val));
+      const id = val.key;
+      const storage = firebase.storage();
+      const storageRef = storage.ref();
+      const filename = new Date().getTime() + '.png';
+      const localPath = pacote.imagem;
+      const fileType = 'image/png';
+      const savedPicture = await storageRef.child(`pacotes/${id}/${filename}`)
+        .putString(localPath, 'base64', { contentType: fileType });
+      const url = await savedPicture.ref.getDownloadURL();
+      console.log(`img url = ${url}`);
+      pacote.imagem = url;
+      await this.afd.object(`pacotes/${id}`).update(pacote);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   getPacotes() {
